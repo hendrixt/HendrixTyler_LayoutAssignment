112a8767a851a80db4e8aafb18572b12d780971c
hendrixt <hendrixt811@gmail.com>
2021-03-20 16:13:39 -0500

    first commit

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -15,71 +15,77 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
-
-    // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
+// TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
     public void calculateTotal(View v){
 
-        double total = 0.0;
+        double total = 0.0 , discount = 0.5;
+        double product_01 = 0.0,  product_02 = 0.0, product_03 = 0.0, product_04 = 0.0;
 
         EditText editText=findViewById(R.id.product_01_input);
         String temp = editText.getText().toString();
-        double product_1 = 0.0;
-        CheckBox p_1_check = (CheckBox)findViewById(R.id.product_01);
 
-        if (p_1_check.isChecked()) {
+        CheckBox p_01_check = (CheckBox)findViewById(R.id.product_01);
+        if (p_01_check.isChecked()) {
             if (temp.length() != 0) {
-                product_1 = Double.parseDouble(temp);
-                total += product_1;
+                product_01 = Double.parseDouble(temp);
+                total += product_01;
             }
         }
 
         editText=findViewById(R.id.product_02_input);
         temp = editText.getText().toString();
-        double product_2 = 0.0;
-        CheckBox p_2_check = (CheckBox)findViewById(R.id.product_02);
 
-        if (p_2_check.isChecked()) {
+        CheckBox p_02_check = (CheckBox)findViewById(R.id.product_02);
+
+        if (p_02_check.isChecked()) {
+
+
             if (temp.length() != 0) {
-                product_2 = Double.parseDouble(temp);
-                total += product_2;
+                product_02 = Double.parseDouble(temp);
+                total += product_02;
             }
         }
 
         editText=findViewById(R.id.product_03_input);
         temp = editText.getText().toString();
-        double product_3 = 0.0;
+
         CheckBox p_3_check = (CheckBox)findViewById(R.id.product_03);
 
         if (p_3_check.isChecked()) {
+
+
             if (temp.length() != 0) {
-                product_3 = Double.parseDouble(temp);
-                total += product_3;
+                product_03 = Double.parseDouble(temp);
+                total += product_03;
             }
         }
 
         editText=findViewById(R.id.product_04_input);
         temp = editText.getText().toString();
-        double product_4 = 0.0;
+
         CheckBox p_4_check = (CheckBox)findViewById(R.id.product_04);
 
         if (p_4_check.isChecked()) {
+
+
             if (temp.length() != 0) {
-                product_4 = Double.parseDouble(temp);
-                total += product_4;
+                product_04 = Double.parseDouble(temp);
+                total += product_04;
             }
         }
 
+// TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
+
         MaterialButtonToggleGroup discount_group = findViewById(R.id.discount_group);
-        Integer checkedBtnIndex=discount_group.getCheckedButtonId();
-        Double discount = 0.5;
+        Integer checkedBtnIndex = discount_group.getCheckedButtonId();
+
 
         if(checkedBtnIndex == findViewById(R.id.discount_bttn).getId()){
             total = total * discount;
